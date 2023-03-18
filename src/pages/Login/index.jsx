7fa150532e0ee364998ef8e6f174e126cc2c60b2
plfmoura <plfmoura96@gmail.com>
2023-03-18 18:24:50 -0300

    add(login):azul tour logo/animations

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import FormAuth from "./FormAuth";
-import './login.css'
+import "./login.css";
+import AzulLogo from "../../../public/azul.png";
 
 export default function Login() {
- 
   return (
     <div className="Login-container">
+      <div className="azul-logo">
+        <img 
+          src={AzulLogo} 
+          alt="Logo da empresa Azul Tour" 
+          />
+        <span>Azul Tour</span>
+      </div>
       <FormAuth />
     </div>
   );
