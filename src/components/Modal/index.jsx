153ea57e2700a9b39a8766cdfdd4ce8efcde1a45
plfmoura <plfmoura96@gmail.com>
2023-04-21 23:22:13 -0300

    add(FAQ): axios request/show questions

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 
 export default function OurModal(props) {
+
   return (
     <Modal
       {...props}
--- src/pages/FAQ/index.jsx
+++ src/pages/FAQ/index.jsx
@@ -1,17 +1,30 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { Button } from "react-bootstrap";
 import { CardContext } from "../../context/CardContext";
 import { RiSearchLine } from "react-icons/ri";
 import './faq.css'
 import FAQCard from './FAQCard';
+import { useSelector } from 'react-redux';
+import PreLoader from '../../assets/Animations/PreLoader'
 
 export default function FAQ() {
   const { handleClean, setQuestions } = useContext(CardContext)
+  const state = useSelector((state) => state);
+  const [data, setData] = useState();
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     handleClean()
     setQuestions(true)
-  } ,[])
+  }, [])
+
+  useEffect(() => {
+    setData(state.faq.faq)
+    setTimeout(() => {
+      setLoading(false)
+      console.log('opa')
+    }, [3000])
+  }, [state])
 
   return (
     <div className="Faq-container">
@@ -28,9 +41,19 @@ export default function FAQ() {
           <Button variant="primary"><RiSearchLine /></Button>
         </div>
       </header>
-      <section className="faq-content">
-        <FAQCard status={true}/>
-      </section>
+      {!loading ?
+        <section className="faq-content">
+          {
+            data.map((question, key) =>
+              <FAQCard
+                status={question.response != "" ? true : false}
+                text={question.question}
+                key={key}
+              />
+            )
+          }
+        </section> : <PreLoader />
+      }
     </div>
   )
 }
